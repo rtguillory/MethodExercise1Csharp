2eb77733ba9060f2f9b00390f69763d7c0cfb49d
Rhonda Guillory <guilloryrt.ps@gmail.com>
2022-02-04 22:42:52 -0600

    added comments

--- MethodExercise/Program.cs
+++ MethodExercise/Program.cs
@@ -1,8 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 //
 
-
+//Exercise 1
+//Abstracted into methods
 Story();
+
+//Exercise 2 -- call each method and print results
 var sum = Add(2, 3, 4);
 Console.WriteLine($"Sum: {sum}");
 Console.WriteLine($"Difference: {Subtract(1, 2)}");
@@ -11,6 +14,7 @@ Console.WriteLine($"Product: {Multiply(1, 2, 3)}");
 Console.WriteLine($"Quotient: {Divide(17, 2)}");
 Console.WriteLine($"Modulus: {Modulus(12,5)}");
 
+//Ex 1 -- //Ex 1 -- method to get user's favorite color
 static string Color()
 {
     Console.WriteLine("What is your favorite color?");
@@ -19,7 +23,7 @@ static string Color()
         return favoriteColor;
     return "";
 }
-
+//Ex 1 -- method to get  user name
 static string Name()
 {
     Console.WriteLine("What is your name?");
@@ -28,7 +32,7 @@ static string Name()
         return "";
     return name;
 }
-
+//Ex 1 -- method to get user's favorite band
 static string Band()
 {
     Console.WriteLine("What is your favorite band?");
@@ -37,7 +41,7 @@ static string Band()
         return "";
     return favoriteBand;
 }
-
+//Ex 1 -- method to get user's favorite animal
 static string Animal()
 {
     Console.WriteLine("What is your favorite animal?");
@@ -46,7 +50,7 @@ static string Animal()
         return "";
     return favoriteAnimal;
 }
-
+//Ex 1 -- call methods for name and favorite things and create story
 static void Story()
 {
     var favoriteColor = Color();
@@ -58,7 +62,7 @@ static void Story()
 }
 
 
-
+//Ex 2 -- method to find sum using params key word
 static int Add(params int[] nums)
 {
     int sum = 0;
@@ -69,11 +73,13 @@ static int Add(params int[] nums)
     return sum;
 }
 
+//Ex 2 -- method to find difference of 2 numbers
 static int Subtract(int a, int b)
 {
     return a-b;
 }
 
+//Ex 2 -- method to find product using params key word
 static int Multiply(params int[] nums)
 {
     int product = 0;
@@ -84,12 +90,13 @@ static int Multiply(params int[] nums)
     return product;
 }
 
+//Ex 2 -- method to find quotient of 2 doubles.
 static double Divide(double a, double b)
 {
     return a/b;
 }
 
-
+//Ex 2 -- method to find modulus for 2 ints
 static int Modulus(int a, int b)
 {
     return a % b;
